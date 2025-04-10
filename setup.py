9bb8b902bfbbd2332e008df533b00f9ffeb9cd7f
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-04-10 12:09:16 +0200

    test

--- setup.py
+++ setup.py
@@ -13,7 +13,9 @@ _deps = [
     "controlnet-aux==0.0.9",
     "huggingface_hub",
     "numpy",
-    "peft"
+    "peft",
+    "ninja",
+    "triton"
 ]
 
 deps = {b: a for a, b in (re.findall(r"^(([^!=<>~]+)(?:[!=<>~].*)?$)", x)[0] for x in _deps)}
@@ -28,7 +30,8 @@ install_requires = [
     deps["huggingface_hub"],
     deps["numpy"],
     deps["peft"],
-    "stable-fast @ git+https://github.com/FlyMyAI/stable-fast.git"
+    "git+https://github.com/FlyMyAI/stable-fast.git",
+    deps["ninja"]
 ]
 
 setup(
