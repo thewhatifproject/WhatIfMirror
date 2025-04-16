3b08704e8f98d39d3bd45e04cc1477d5511d0b0f
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-04-16 10:43:47 +0200

    dd

--- setup.py
+++ setup.py
@@ -1,58 +1,65 @@
+import os
 import re
 
 from setuptools import find_packages, setup
 
+
 _deps = [
     "torch",
-    "diffusers",
+    "diffusers>=0.29.0",
     "transformers",
     "accelerate",
     "fire",
-    "omegaconf",
-    "pywin32;sys_platform == 'win32'",
-    "controlnet-aux==0.0.9",
-    "huggingface_hub",
-    "numpy",
-    "peft"
-    ]
+    # "omegaconf",
+    "cuda-python",
+    "onnx==1.15.0",
+    "onnxruntime==1.16.3",
+    "onnx_graphsurgeon>=0.5.2",
+    "protobuf==3.20.2",
+    "colored",
+    "xformers",
+    "pywin32;sys_platform == 'win32'"
+]
 
 deps = {b: a for a, b in (re.findall(r"^(([^!=<>~]+)(?:[!=<>~].*)?$)", x)[0] for x in _deps)}
 
+
+def deps_list(*pkgs):
+    return [deps[pkg] for pkg in pkgs]
+
+
+extras = {}
+extras["xformers"] = deps_list("xformers")
+extras["torch"] = deps_list("torch", "accelerate")
+extras["tensorrt"] = deps_list("protobuf", "cuda-python", "onnx", "onnxruntime", "colored")
+
+extras["dev"] = extras["xformers"] + extras["torch"] + extras["tensorrt"]
+
 install_requires = [
     deps["fire"],
-    deps["omegaconf"],
+    # deps["omegaconf"],
     deps["diffusers"],
     deps["transformers"],
     deps["accelerate"],
-    deps["controlnet-aux"],
-    deps["huggingface_hub"],
-    deps["numpy"],
-    deps["peft"]
+    "compel @ git+https://github.com/oylo-io/compel.git#egg=compel"
 ]
 
 setup(
-    name="whatifmirror",
-    version="0.1.0",
-    description="Real-time interactive image generation pipeline based on a customized version of StreamDiffusion",
+    name="streamdiffusion",
+    version="0.1.91",
+    description="real-time interactive image generation pipeline",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     keywords="deep learning diffusion pytorch stable diffusion audioldm streamdiffusion real-time",
     license="Apache 2.0 License",
-    author="Daniele Giannini",
-    author_email="contact.thewhatifproject@gmail.com",
-    url="https://github.com/thewhatifproject/WhatIfMirror",
+    author="Aki, kizamimi, ddPn08, Verb, ramune, teftef6220, Tonimono, Chenfeng Xu, Ararat with the help of all our contributors (https://github.com/cumulo-autumn/StreamDiffusion/graphs/contributors)",
+    author_email="cumulokyoukai@gmail.com",
+    url="https://github.com/cumulo-autumn/StreamDiffusion",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    package_data={"whatifmirror": ["py.typed"]},
+    package_data={"streamdiffusion": ["py.typed"]},
     include_package_data=True,
     python_requires=">=3.10.0",
     install_requires=list(install_requires),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent"
-    ],
-    project_urls={
-        "Original project (StreamDiffusion)": "https://github.com/cumulo-autumn/StreamDiffusion"
-    }
+    extras_require=extras,
 )
