30fbfe9a11085de236e935965efd7dab0e3c8965
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-04-17 11:02:46 +0200

    d

--- src/whatifmirror/utils/wrapper.py
+++ src/whatifmirror/utils/wrapper.py
@@ -366,7 +366,7 @@ class WhatIfMirrorWrapper:
             seed = np.random.randint(0, 1000000)
 
         stream.prepare(
-            num_inference_steps=2,
+            num_inference_steps=self.original_inference_steps,
             seed=seed,
         )
 
